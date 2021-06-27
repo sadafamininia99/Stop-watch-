1afd1719392e99e843baf2e1d599831bc3c0a985
SADAF <66588239+sadafamininia99@users.noreply.github.com>
2021-06-27 14:48:45 +0430

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+        const startButton = document.querySelector('[data-action="start"]');
+        const stopButton  = document.querySelector('[data-action="stop"]');
+        const resetButton  = document.querySelector('[data-action="reset"]');
+        const minutes       = document.querySelector('.minutes');
+        const seconds        = document.querySelector('.seconds');
+
+
+     //ADD E
