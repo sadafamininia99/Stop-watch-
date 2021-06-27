8c8adf91b0e17dc9007686a60e97ca0a790ed452
SADAF <66588239+sadafamininia99@users.noreply.github.com>
2021-06-27 15:45:55 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-const startButton = document.querySelector('[data-action="start"]');
-const stopButton = document.querySelector('[data-action="stop"]');
-const resetButton = document.querySelector('[data-action="reset"]');
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
-let timerTime = 57;
+    const startButton = document.querySelector('[data-action="start"]');
+    const stopButton  = document.querySelector('[data-action="stop"]');
+    const resetButton = document.querySelector('[data-action="reset"]');
+    const minutes     = document.querySelector('.minutes');
+    const seconds     = document.querySelector('.seconds');
+    let timerTime     = 00;
+    let isRunning     = false;
+    let interval;    
 
-//interval
+    function startTimer() {
+      if (isRunning) return;
 
-setInterval(function () {
-    timerTime++;
+      isRunning = true;
+      interval  = setInterval(incrementTimer, 1000);
+    }
 
-    const numOfMInutes = Math.floor(timerTime /60);
-    const numOfSeconds = timerTime % 60; 
+    function stopTimer() {
+      if (!isRunning) return;
 
-    console.log(numOfMInutes , numOfSeconds);
+      isRunning = false;
+      clearInterval(interval);
+    }
 
-    seconds.innerText =numOfSeconds;
-    // console.count("helooooo");
-    },1000);
+    function resetTimer() {
+      stopTimer();
 
-//Functions👩🏼‍🦯👩🏼‍🦯
+      timerTime = 0;
+      minutes.innerText = '00';
+      seconds.innerText = '00';
+    }
 
-function startTimer() {}
+    function incrementTimer() {
+      timerTime++;
 
-function stopTimer() {}
+      const numOfMinutes = Math.floor(timerTime / 60);
+      const numOfSeconds = timerTime % 60;
 
-function resetTimer() {}
+      minutes.innerText = pad(numOfMinutes);
+      seconds.innerText = pad(numOfSeconds);
+    }
 
-//ADD EventListiner
-startButton.addEventListener("click", startTimer);
-stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
+    function pad(number) {
+      return (number < 10) ? '0' + number : number;
+      // if (number < 10) {
+      //   return '0' + number;
+      // } else {
+      //   return number;
+      // }
+    }
+
+    startButton.addEventListener('click', startTimer);
+    stopButton.addEventListener('click', stopTimer);
+    resetButton.addEventListener('click', resetTimer);
